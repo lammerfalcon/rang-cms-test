0ab0920e26cd1b08a3da6557bdf623d43bd68d15
rodiongavrilov <rodiongavrilov@gmail.com>
2023-07-19 10:59:06 +0300

    init

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,8 @@
 <template>
     <header class="w-full h-24">
-      <div class="container h-full mx-auto flex items-center justify-between">
+      <div class="container px-2 h-full mx-auto flex items-center justify-between">
         <NuxtLink to="/">
-          <h1 class="text-gray-800 text-3xl font-bold">rodion gavrilov</h1>
+          <h1 class="text-gray-800 sm:text-4xl text-xl font-bold">rodion gavrilov</h1>
         </NuxtLink>
         <nav>
           <ul class="flex space-x-8 text-lg font-bold">
