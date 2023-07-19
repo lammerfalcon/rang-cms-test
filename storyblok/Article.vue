2670d7e58b3192010ccded6fbf79b771778516d8
rodiongavrilov <rodiongavrilov@gmail.com>
2023-07-19 10:52:57 +0300

    init

--- storyblok/Article.vue
+++ storyblok/Article.vue
@@ -2,15 +2,16 @@
     <div v-editable="blok">
       <img
         :src="blok.iamge + '/m/1600x0'"
-        class="mx-auto w-3/4 object-cover"
+        class="mx-auto w-3/4 object-cover mb-10"
       />
-      <div class="container mx-auto mb-12">
-        <h1 class="text-6xl text-gray-800 font-bold mt-12 mb-4">{{ blok.title }}</h1>
+      <div class="container mx-auto mb-12 columns-2">
+        <h1 class="text-4xl text-gray-800 font-bold mb-4">{{ blok.title }}</h1>
         <h2 class="text-2xl text-gray-500 font-bold mb-4">
           {{ blok.description }}
         </h2>
-        <div class="text-gray-600 mb-3">Written by: <b>{{ blok.author }}</b></div>
         <div v-html="resolvedRichText"></div>
+
+        <p class="text-gray-600 max-w-xl my-3 text-[0.7rem]">Written by: <b>{{ blok.author }}</b></p>
       </div>
     </div>
   </template>
